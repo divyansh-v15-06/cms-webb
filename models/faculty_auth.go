ee03a6b755cd6ec7385998fb45365531fa406bff
ayush00git <ayush.2007.iit@gmail.com>
2026-04-13 01:40:06 +0530

    feat: added enumerated types for hostel and dep types

--- models/faculty_auth.go
+++ models/faculty_auth.go
@@ -23,17 +23,33 @@ const (
 	Type5 BlockType = "5"
 )
 
-// set one enumerated type for department as well
+type DepartmentName string
+const (
+	CSE  DepartmentName = "Computer Science & Engineering"
+	CE   DepartmentName = "Civil Engineering"
+	CHE  DepartmentName = "Chemical Engineering"
+	ECE  DepartmentName = "Electronics & Communication Engineering"
+	EE   DepartmentName = "Electrical Engineering"
+	ME   DepartmentName = "Mechanical Engineering"
+	MSE  DepartmentName = "Material Science & Engineering"
+	CHEM DepartmentName = "Chemistry"
+	MSC  DepartmentName = "Mathematics & Scientific Computing"
+	PPS  DepartmentName = "Physics & Photonics Science"
+	ARCH DepartmentName = "Architecture"
+	HSS  DepartmentName = "Humanities & Social Sciences"
+	MS   DepartmentName = "Management Studies"
+	CES  DepartmentName = "Centre For Energy Studies"
+)
 
 type Faculty struct {
 	ID					uint  			`gorm:"primaryKey;autoIncrement"`
 	Name				string			`gorm:"not null"`
-	Email				string			`gorm:"uniqueIndex; not null"`
-	Department			string			`gorm:"not null"`
+	Email				string			`gorm:"uniqueIndex;not null"`
+	Department			DepartmentName	`gorm:"not null"`
 	HouseNumber			string			`gorm:"not null"`
 	Block				BlockLabel		`gorm:"type:char(1);not null"`
 	Type				BlockType		`gorm:"type:char(1);not null"`
-	PhoneNumber			string			`gorm:"uniqueIndex;not null"`
+	PhoneNumber			string			`gorm:"type:char(10);uniqueIndex;not null"`
 	IsVerified			bool  			`gorm:"default:false"`
 	CreatedAt			time.Time		
 }
--- models/warden_auth.go
+++ models/warden_auth.go
@@ -6,15 +6,27 @@ import (
 
 type HostelName string
 const (
-	// mention all hostel names
+	KBH 		HostelName = "Kailash Boys Hostel"
+	HBH 	 	HostelName = "Himadri Boys Hostel"
+	HimgiriBH	HostelName = "Himgiri Boys Hostel"
+	UBH 		HostelName = "Udaygiri Boys Hostel"
+	NBH 		HostelName = "Neelkanth Boys Hostel"
+	DBH 		HostelName = "Dhauladhar Boys Hostel"
+	VBH 		HostelName = "Vindhyachal Boys Hostel"
+	SBH 		HostelName = "Shivalik Boys Hostel"
+	SAT 		HostelName = "Satpura Hostel"
+	AGH 		HostelName = "Ambika Girls Hostel"
+	PGH 		HostelName = "Parvati Girls Hostel"
+	MGH 		HostelName = "Mani-Mahesh Girls Hostel"
+	ARG 		HostelName = "Aravali Girls Hostel"
 )
 
 type Warden struct {
 	ID					uint			`gorm:"primaryKey;autoIncrement"`
 	Name				string			`gorm:"not null"`
-	Email				string			`gorm:"uniqueIndex; not null"`
-	Hostel				HostelName		`gorm:"type:text; not null"`
-	PhoneNumber			string			`gorm:"uniqueIndex; not null"`
+	Email				string			`gorm:"uniqueIndex;not null"`
+	Hostel				HostelName		`gorm:"type:text;not null"`
+	PhoneNumber			string			`gorm:"type:char(10);uniqueIndex;not null"`
 	IsVerified			bool			`gorm:"default:false"`
 	CreatedAt			time.Time
 }
